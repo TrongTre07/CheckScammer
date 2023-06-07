0b9ee7d569f0df0d78bfc92c7e4a6164918f16c7
tonynhox <anhhuytran1470@gmail.com>
2023-06-07 22:37:13 +0700

    Update HomeScreen.js

--- src/app/screens/HomeScreen.js
+++ src/app/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 // HomeScreen.js
 
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {
   View,
   TextInput,
@@ -9,17 +9,18 @@ import {
   StyleSheet,
   Pressable,
   Image,
-  ImageBackground,
+  RefreshControl,
+  ScrollView,
 } from 'react-native';
-import { useMyContext } from '../MyContext';
+import {useMyContext} from '../MyContext';
 import instance from '../../axios/AxiosInstance';
 import Toast from 'react-native-toast-message';
 
-const HomeScreen = ({ navigation }) => {
-  const { allNumber, setAllNumber } = useMyContext();
+const HomeScreen = ({navigation}) => {
+  const {allNumber, setAllNumber} = useMyContext();
 
   const sendId = id => {
-    navigation.navigate('Details', { idItem: id });
+    navigation.navigate('Details', {idItem: id});
   };
 
   useEffect(() => {
@@ -80,7 +81,7 @@ const HomeScreen = ({ navigation }) => {
     return dateRender;
   };
 
-  const { userData } = useMyContext();
+  const {userData} = useMyContext();
   const goAddReport = () => {
     if (userData == undefined) {
       Toast.show({
@@ -98,31 +99,22 @@ const HomeScreen = ({ navigation }) => {
       <>
         {allNumber ? (
           <>
-            <ImageBackground
-              source={require('../../media/imgBackground/bgHorizontal.png')}
-              style={styles.searchBox}
-            >
-              <View />
-              <View style={{ position: 'relative' }}>
-                <TextInput
-                  style={styles.searchInput}
-                  placeholder="Enter phone number"
+            <View style={styles.searchBox}>
+              <TextInput
+                style={styles.searchInput}
+                placeholder="Enter phone number"
                 // Implement the logic to handle user input for searching
-                />
-                <Image source={require('../../media/iconApp/search.png')}/>
-              </View>
-
+              />
               <Pressable onPress={() => goAddReport()}>
                 <Image source={require('../../media/iconApp/report.png')} />
               </Pressable>
-            </ImageBackground>
-
+            </View>
             <FlatList
               refreshControl={
                 <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
               }
               data={allNumber}
-              renderItem={({ item }) => (
+              renderItem={({item}) => (
                 <Pressable onPress={() => sendId(item._id)}>
                   <View style={styles.phoneNumberItem}>
                     <Text style={styles.phoneNumberText}>
@@ -133,24 +125,18 @@ const HomeScreen = ({ navigation }) => {
                       <Text style={styles.ownerText}>
                         {convertTime(item.date)}
                       </Text>
-                      <View style={styles.nameAndDate}>
-                        <Text style={styles.ownerText}>{item.name}</Text>
-                        <Text style={styles.ownerText}>
-                          {convertTime(item.date)}
-                        </Text>
-                      </View>
                     </View>
-                    </View>
+                  </View>
                 </Pressable>
               )}
-               keyExtractor={item => item._id}
+              keyExtractor={item => item._id}
+              // Implement the logic to fetch and display the phone numbers and their owners
             />
             <View style={styles.totalScammedContainer}>
               <Text style={styles.totalScammedText}>
                 Tổng số điện thoại Scam: {allNumber.length}
               </Text>
             </View>
-
           </>
         ) : (
           <Text>Nothing to show...</Text>
@@ -163,17 +149,10 @@ const HomeScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  imageBG: {
-    width: '100%',
-    height: 40,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
   searchBox: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height: 70,
   },
   totalScammedContainer: {
     flexDirection: 'row',
@@ -187,13 +166,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5F5F5',
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
   searchInput: {
-    width: '83%',
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
     borderColor: '#CCCCCC',
-    borderRadius: 5,
+    borderRadius: 8,
+    marginBottom: 10,
+    paddingHorizontal: 10,
     backgroundColor: '#FFFFFF',
-    height: '60%'
   },
   phoneNumberItem: {
     marginBottom: 10,
@@ -205,10 +189,9 @@ const styles = StyleSheet.create({
   phoneNumberText: {
     fontWeight: 'bold',
     fontSize: 20,
-    color: '#3F4A71'
   },
   ownerText: {
-    color: '#3F4A71',
+    color: '#888888',
     fontWeight: 'bold',
     fontSize: 16,
   },
@@ -216,7 +199,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 10,
     alignSelf: 'flex-end',
-    color: '#3F4A71',
+    color: '#888888',
   },
 });
 
